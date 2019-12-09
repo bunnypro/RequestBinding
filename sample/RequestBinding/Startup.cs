88b284276d3ef2bd94ea66cb5dea5d610d629183
Mubarrid Albar <mubarrid.albar@outlook.com>
2019-12-09 18:45:47 +0700

    adjustment

--- sample/RequestBinding/Startup.cs
+++ sample/RequestBinding/Startup.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Bunnypro.RequestBinding.Samples.WebApi
+namespace Bunnypro.RequestBinding.Sample
 {
     public class Startup
     {
